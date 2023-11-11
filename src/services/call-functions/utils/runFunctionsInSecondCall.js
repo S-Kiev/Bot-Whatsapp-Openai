898e6c096dae3d643b7e2b16d4151c3c4ebc56bf
S-Kiev <vezequiel58@gmail.com>
2023-11-11 12:28:01 -0300

    integrando endpoits de strapi al bot

--- src/services/call-functions/utils/runFunctionsInSecondCall.js
+++ src/services/call-functions/utils/runFunctionsInSecondCall.js
@@ -35,7 +35,7 @@ async function runFunctionsInSecondCall (userText, argumentsFunction, nameFuncti
         });
     
     
-        console.log("Razon de fin de la segunda llamada: " + response.choices[0].finish_reason)
+        console.log(response);
     
         if(response.choices[0].finish_reason === "stop") {
             console.log(response.choices[0].message.content)
@@ -49,3 +49,75 @@ async function runFunctionsInSecondCall (userText, argumentsFunction, nameFuncti
 module.exports = {
     runFunctionsInSecondCall
 }
+
+/*
+async function runCallFunctionsSecond (userText, argumentsFunction, nameFunction, Obj) {
+
+    console.log(userText);
+    console.log(argumentsFunction);
+    console.log(nameFunction);
+    console.log(Obj);
+    
+    
+        const response = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo-0613',
+            "messages": [
+                {"role": "user", "content": userText},
+                {
+                    "role": "assistant",
+                    "content": null,
+                    "function_call": {
+                      "name": "get_current_weather",
+                      "arguments": argumentsFunction
+                    }
+                },
+                {
+                "role": "function",
+                "name" : nameFunction,
+                "content": JSON.stringify(Obj),
+                }
+              ],
+              "functions": [
+                {
+                  "name": "get_current_weather",
+                  "description": "Get the current weather in a given location",
+                  "parameters": {
+                    "type": "object",
+                    "properties": {
+                      "location": {
+                        "type": "string",
+                        "description": "The city and state, e.g. San Francisco, CA"
+                      },
+                      "unit": {
+                        "type": "string",
+                        "enum": ["celsius", "fahrenheit"]
+                      }
+                    },
+                    "required": ["location"]
+                  }
+                },
+                {
+                    name: "getTimeOfDay",
+                    description: "Get the time of day.",
+                    parameters: {
+                        type: "object",
+                        properties: {
+                        },
+                        require: [],
+                    }
+                }
+              ]
+        });
+    
+    
+    
+    
+        if(response.choices[0].finish_reason === "stop") {
+            console.log(response.choices[0].message.content)
+            return response.choices[0].message.content;
+        } else {
+          return `Ups parace que algo ha salido mal al llamar a la funcion: ${nameFunction}`;
+        }
+    
+    }
+ */   
