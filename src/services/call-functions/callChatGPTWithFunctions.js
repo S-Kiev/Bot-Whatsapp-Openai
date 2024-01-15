858e04305bd9af22f3c28f164b3b7f0b79daf4df
S-Kiev <vezequiel58@gmail.com>
2024-01-15 17:43:20 -0300

    haciendo reset antes de Gemini

--- src/services/call-functions/callChatGPTWithFunctions.js
+++ src/services/call-functions/callChatGPTWithFunctions.js
@@ -8,36 +8,36 @@ const { runFunctionsInSecondCall } = require('./utils/runFunctionsInSecondCall')
 const openai = new OpenAI({});
 
 
-async function runCallFunctions (userText, number) {
+async function runCallFunctions(userText, number) {
 
   let response = null;
- 
+
+  try {
+
     response = await openai.chat.completions.create({
-        model: 'gpt-3.5-turbo-0613',
-        "messages": [
-            {
-              "role": "system",
-              "content": "Tu nombre es Alicia, eres un bot asistente de una clinica estetica-medica que brinda varios tratamientos. Eres muy util y profesional ayudando a los miebros de la clinica a buscar información, actualizarla y agendar consultas"
-            },
-            {
-               "role": "user",
-               "content": userText
-            }
-          ],
-        "functions": functionDictionary
+      model: 'gpt-3.5-turbo-0613',
+      "messages": [
+        {
+          "role": "system",
+          "content": "Tu nombre es Alicia, eres un bot asistente de una clinica estetica-medica que brinda varios tratamientos. Eres muy util y profesional ayudando a los miebros de la clinica a buscar información, actualizarla y agendar consultas"
+        },
+        {
+          "role": "user",
+          "content": userText
+        }
+      ],
+      "functions": functionDictionary
     });
 
-console.log(response);
-
-console.log(response.choices[0].message.function_call.name);
 
     if (response.choices[0].finish_reason === "stop") {
-      console.log(response.choices[0].message.content);
+      console.log("Este es el mensaje: ");
+      console.log(response.choices[0].message);
       return response.choices[0].message.content;
     }
 
-    
-    const {name: nameFunction, arguments: argumentsFunction} = response.choices[0].message.function_call;
+
+    const { name: nameFunction, arguments: argumentsFunction } = response.choices[0].message.function_call;
     const parsedArguments = JSON.stringify(argumentsFunction);
     const objectArguments = JSON.parse(argumentsFunction);
 
@@ -48,11 +48,11 @@ console.log(response.choices[0].message.function_call.name);
     let data;
 
     console.log(response.choices[0].finish_reason);
-    
+
     //Quiero que identifiques a Juan Perez
     if (nameFunction === 'findCustomerByNameAndLastname') {
 
-      const name  = objectArguments.name.replace(/\s/g, "%20");
+      const name = objectArguments.name.replace(/\s/g, "%20");
       const lastname = objectArguments.lastname.replace(/\s/g, "%20");
 
       url = process.env.STRAPI_BACKEND_HOST + (`/api/customer-personal-informations?filters[name][$eqi]=${name}&filters[lastname][$eqi]=${lastname}`);
@@ -61,8 +61,8 @@ console.log(response.choices[0].message.function_call.name);
     //quiero encontrar al usuario responsable Ezequiel Viera
     else if (nameFunction === 'findConsultationByResponsibleUserName') {
 
-      const name  = objectArguments.name.replace(/\s/g, "%20");
-      const lastname  = objectArguments.lastname.replace(/\s/g, "%20");
+      const name = objectArguments.name.replace(/\s/g, "%20");
+      const lastname = objectArguments.lastname.replace(/\s/g, "%20");
 
       url = process.env.STRAPI_BACKEND_HOST + (`/api/consultations?populate=*&filters[responsibleUser][name][$eqi]=${name}&filters[responsibleUser][lastname][$eqi]=${lastname}&sort[0]=createdAt:desc&pagination[limit]=1`);
     }
@@ -72,38 +72,38 @@ console.log(response.choices[0].message.function_call.name);
       url = process.env.STRAPI_BACKEND_HOST + (`/api/consultation-consulting-rooms?populate[consultation][populate][0]=customer&populate[consultation][populate][1]=responsibleUser&filters[since][$gte]=${objectArguments.dateSince}&filters[since][$lte]=${objectArguments.dateUntil}&filters[consultation][responsibleUser][cellphone][$eq]=${number}`);
     }
 
-    
+
     //No funciona aun
     else if (nameFunction === 'findConsultationByCustomerNameAndLastname') {
 
-      const name  = objectArguments.name.replace(/\s/g, "%20");
-      const lastname  = objectArguments.lastname.replace(/\s/g, "%20");
+      const name = objectArguments.name.replace(/\s/g, "%20");
+      const lastname = objectArguments.lastname.replace(/\s/g, "%20");
 
       url = process.env.STRAPI_BACKEND_HOST + (`/api/consultations?populate=*&filters[customer][name][$eqi]=${name}&filters[customer][lastname][$eqi]=${lastname}&sort[0]=createdAt:desc&pagination[limit]=1`);
     }
-    
+
     //quiero saber como se encunetra el estado de pagos de Emilio Perez
     else if (nameFunction === 'findStatusOfPaymentsOfCustomerByNameAndLastname') {
 
-      const name  = objectArguments.name.replace(/\s/g, "%20");
-      const lastname  = objectArguments.lastname.replace(/\s/g, "%20");
+      const name = objectArguments.name.replace(/\s/g, "%20");
+      const lastname = objectArguments.lastname.replace(/\s/g, "%20");
 
       url = process.env.STRAPI_BACKEND_HOST + (`/api/customer-payments?populate=*&filters[customer][name][$eqi]=${name}&filters[customer][lastname][$eqi]=${lastname}&sort[0]=createdAt:desc&pagination[limit]=1`);
     }
 
-        //quiero saber cuanto debe en total Emilio Perez
-        else if (nameFunction === 'findTotalDebtByCustomerNameAndLastname') {
+    //quiero saber cuanto debe en total Emilio Perez
+    else if (nameFunction === 'findTotalDebtByCustomerNameAndLastname') {
 
-          const name  = objectArguments.name.replace(/\s/g, "%20");
-          const lastname  = objectArguments.lastname.replace(/\s/g, "%20");
-    
-          url = process.env.STRAPI_BACKEND_HOST + (`/api/customer-payments?filters[paymentStatus][$eqi]=partial&filters[paymentStatus][$eqi]=pending&filters[customer][name][$eqi]=${name}&filters[customer][lastname][$eqi]=${lastname}`);
-        }
+      const name = objectArguments.name.replace(/\s/g, "%20");
+      const lastname = objectArguments.lastname.replace(/\s/g, "%20");
+
+      url = process.env.STRAPI_BACKEND_HOST + (`/api/customer-payments?filters[paymentStatus][$eqi]=partial&filters[paymentStatus][$eqi]=pending&filters[customer][name][$eqi]=${name}&filters[customer][lastname][$eqi]=${lastname}`);
+    }
 
     //quiero encontrar el equipo Melashade
     else if (nameFunction === 'findEquitmentByName') {
 
-      const name  = objectArguments.name.replace(/\s/g, "%20");
+      const name = objectArguments.name.replace(/\s/g, "%20");
 
       url = process.env.STRAPI_BACKEND_HOST + (`/api/equipments?populate=*&filters[name][$eqi]=${name}`);
     }
@@ -112,7 +112,7 @@ console.log(response.choices[0].message.function_call.name);
     //Problema => hay 2 melashade en los registros, confunde a GPT
     else if (nameFunction === 'findByStatusEquitmentByName') {
 
-      const name  = objectArguments.name.replace(/\s/g, "%20");
+      const name = objectArguments.name.replace(/\s/g, "%20");
 
       url = process.env.STRAPI_BACKEND_HOST + (`/api/equipment-histories?populate=*&filters[equipment][name][$eqi]=${name}`);
     }
@@ -120,7 +120,7 @@ console.log(response.choices[0].message.function_call.name);
     //quiero saber sobre el tratamiento masajes
     else if (nameFunction === 'findTreatmentsByName') {
 
-      const name  = objectArguments.name.replace(/\s/g, "%20");
+      const name = objectArguments.name.replace(/\s/g, "%20");
 
       url = process.env.STRAPI_BACKEND_HOST + (`/api/treatments?populate=*&filters[name][$eqi]=${name}`);
     }
@@ -128,7 +128,7 @@ console.log(response.choices[0].message.function_call.name);
     //quiero saber sobre el consultorio que es la sala de melashade
     else if (nameFunction === 'findConsultingRoomsByName') {
 
-      const name  = objectArguments.name.replace(/\s/g, "%20");
+      const name = objectArguments.name.replace(/\s/g, "%20");
 
       url = process.env.STRAPI_BACKEND_HOST + (`/api/consulting-rooms?populate=*&filters[name][$eqi]=${name}`);
     }
@@ -136,7 +136,7 @@ console.log(response.choices[0].message.function_call.name);
     //quiero saber sobre el estado del consultorio que es la sala de melashade
     else if (nameFunction === 'findStatusOfConsultingRoomsByName') {
 
-      const name  = objectArguments.name.replace(/\s/g, "%20");
+      const name = objectArguments.name.replace(/\s/g, "%20");
 
       url = process.env.STRAPI_BACKEND_HOST + (`/api/consulting-room-histories?populate=*&filters[consulting_room][name][$eqi]=${name}`);
     }
@@ -144,7 +144,7 @@ console.log(response.choices[0].message.function_call.name);
     //quiero saber sobre la informacion medica de Juan Perez
     else if (nameFunction === 'findMedicalInformationOfCustomerByNameAndLastname') {
 
-      const name  = objectArguments.name.replace(/\s/g, "%20");
+      const name = objectArguments.name.replace(/\s/g, "%20");
       const lastname = objectArguments.lastname.replace(/\s/g, "%20");
 
       url = process.env.STRAPI_BACKEND_HOST + (`/api/customer-medical-informations?populate=*&filters[customer][name][$eqi]=${name}&filters[customer][lastname][$eqi]=${lastname}`);
@@ -153,7 +153,7 @@ console.log(response.choices[0].message.function_call.name);
     //quiero saber sobre la las medidas de Juan Perez
     else if (nameFunction === 'findMeasurementsOfCustomerByNameAndLastname') {
 
-      const name  = objectArguments.name.replace(/\s/g, "%20");
+      const name = objectArguments.name.replace(/\s/g, "%20");
       const lastname = objectArguments.lastname.replace(/\s/g, "%20");
 
       url = process.env.STRAPI_BACKEND_HOST + (`/api/measurements-customers?populate=*&filters[customer][name][$eqi]=${name}&filters[customer][lastname][$eqi]=${lastname}`);
@@ -162,16 +162,16 @@ console.log(response.choices[0].message.function_call.name);
     //quiero que modifiques la informacion de Juan Perez, su documento es 123, su cumpleaños es el 14 de diciembre de 1970, su celular es 098545412, su email es ejemplo@gmail.com, su direccion es calle Uruguay 2020, y su profesion es dermatologo
     else if (nameFunction === 'updateCustomerPersonalInformation') {
 
-      const name  = objectArguments.name.replace(/\s/g, "%20");
+      const name = objectArguments.name.replace(/\s/g, "%20");
       const lastname = objectArguments.lastname.replace(/\s/g, "%20");
 
       url = process.env.STRAPI_BACKEND_HOST + (`/api/customer-personal-informations?filters[name][$eqi]=${name}&filters[lastname][$eqi]=${lastname}`);
 
-      const customerResponse  = await strapiRequest( url, method, headers );
+      const customerResponse = await strapiRequest(url, method, headers);
       const customer = JSON.parse(customerResponse).data[0];
 
       console.log("Id del cliente =>" + customer.id);
-     if (customer && customer.id) {
+      if (customer && customer.id) {
 
         data = {
           data: objectArguments
@@ -196,16 +196,16 @@ console.log(response.choices[0].message.function_call.name);
     //quiero que modifiques el estado de pagos de Juan Perez, este ha abonado la totalida del servicio, pagando 6000 pesos sobre una cuenta de 6000
     else if (nameFunction === 'updatePaymentInformationCustomer') {
 
-      const name  = objectArguments.name.replace(/\s/g, "%20");
+      const name = objectArguments.name.replace(/\s/g, "%20");
       const lastname = objectArguments.lastname.replace(/\s/g, "%20");
-      
+
       url = process.env.STRAPI_BACKEND_HOST + (`/api/customer-payments?populate=*&filters[customer][name][$eqi]=${name}&filters[customer][lastname][$eqi]=${lastname}&sort[0]=createdAt:desc&pagination[limit]=1`);
 
-      const customerResponse  = await strapiRequest( url, method, headers );
+      const customerResponse = await strapiRequest(url, method, headers);
       const customer = JSON.parse(customerResponse).data[0];
 
       console.log("Id del cliente =>" + customer.id);
-     if (customer && customer.id) {
+      if (customer && customer.id) {
 
         data = {
           data: objectArguments
@@ -230,16 +230,16 @@ console.log(response.choices[0].message.function_call.name);
     //quiero que modifiques los datos medicos de Juan Perez, este consume perifar 400 como medicacion, su numero de mergencia es 095646464, su doctor es Ejemplencio Rodriguez y no tiene problemas de hipertencion
     else if (nameFunction === 'updateMedicalInformationCustomer') {
 
-      const name  = objectArguments.name.replace(/\s/g, "%20");
+      const name = objectArguments.name.replace(/\s/g, "%20");
       const lastname = objectArguments.lastname.replace(/\s/g, "%20");
-      
+
       url = process.env.STRAPI_BACKEND_HOST + (`/api/customer-medical-informations?populate=*&filters[customer][name][$eqi]=${name}&filters[customer][lastname][$eqi]=${lastname}`);
 
-      const customerResponse  = await strapiRequest( url, method, headers );
+      const customerResponse = await strapiRequest(url, method, headers);
       const customer = JSON.parse(customerResponse).data[0];
 
       console.log("Id del cliente =>" + customer.id);
-     if (customer && customer.id) {
+      if (customer && customer.id) {
 
         data = {
           data: objectArguments
@@ -264,18 +264,18 @@ console.log(response.choices[0].message.function_call.name);
     //quiero que modifiques las medidas de Juan Perez, este tiene un alto de cintura de 60cm, mide 1 metro con 70 centimetros, y tiene un vientre bajo de 70 centimetros
     else if (nameFunction === 'updateMeasurementsCustomer') {
 
-      const name  = objectArguments.name.replace(/\s/g, "%20");
+      const name = objectArguments.name.replace(/\s/g, "%20");
       const lastname = objectArguments.lastname.replace(/\s/g, "%20");
-      
+
       url = process.env.STRAPI_BACKEND_HOST + (`/api/measurements-customers?populate=*&filters[customer][name][$eqi]=${name}&filters[customer][lastname][$eqi]=${lastname}`);
 
-      const customerResponse  = await strapiRequest( url, method, headers );
+      const customerResponse = await strapiRequest(url, method, headers);
       const customer = JSON.parse(customerResponse).data[0];
 
       console.log("Id del cliente =>" + customer.id);
-     if (customer && customer.id) {
+      if (customer && customer.id) {
 
-      console.log(objectArguments);
+        console.log(objectArguments);
         data = {
           data: objectArguments
         }
@@ -298,7 +298,7 @@ console.log(response.choices[0].message.function_call.name);
 
     // CREATE
     //quiero que agendes una consulta para Emilio Perez para el 20 de diciembre de 14 a 16 horas, se realizaran estos tratamientos: masajes y Lo que haga el melashade ("Lo que haga el melashade" es el nombre del tratamiento). eN Sala de Melashade
-    else if (nameFunction === 'botCreate'){
+    else if (nameFunction === 'botCreate') {
       //VER SI POST REQUIERE DE ALGUNA OTRA CONFIGURACION
       url = process.env.STRAPI_BACKEND_HOST + ('/api/consultation/botCreate');
       method = 'post';
@@ -309,7 +309,7 @@ console.log(response.choices[0].message.function_call.name);
     }
 
     //quiero que canceles la consulta de Emilio Perez del 6 de diciembre
-    else if (nameFunction === 'cancelConsultation'){
+    else if (nameFunction === 'cancelConsultation') {
       //VER SI POST REQUIERE DE ALGUNA OTRA CONFIGURACION
       url = process.env.STRAPI_BACKEND_HOST + ('/api/consultation/cancelConsultation');
       method = 'put';
@@ -368,7 +368,7 @@ console.log(response.choices[0].message.function_call.name);
 
     }
 
-    else if (method === 'post'){
+    else if (method === 'post') {
       try {
 
         data = {
@@ -382,7 +382,7 @@ console.log(response.choices[0].message.function_call.name);
         console.log("data => " + JSON.stringify(data));
 
 
-        Obj  = await strapiRequest(url, method, headers, data);
+        Obj = await strapiRequest(url, method, headers, data);
 
         console.log("Obj => ");
         console.log(Obj);
@@ -409,9 +409,14 @@ console.log(response.choices[0].message.function_call.name);
     } catch (error) {
       console.error("Error al obtener el objeto:", error);
     }
+  } catch (error) {
+    console.error("Error al comunicarse con la API de OpenAI:", error);
+    return null;
+  }
+
 }
 
 
 module.exports = {
-    runCallFunctions
+  runCallFunctions
 }
